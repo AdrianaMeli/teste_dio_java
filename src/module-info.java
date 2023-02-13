1f5b6adee1b719204d99d0eef1458def4e049b73
Adriana Borges Nogueira <adriana.nogueira@mercadolivre.com>
2023-02-13 17:16:06 -0300

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author adrnogueira
+ *
+ */
+module primeiro_programa {
+}
---
+++ src/primeiro_programa/Primeiro_Programa.java
@@ -0,0 +1,20 @@
+package primeiro_programa;
+
+import primeiro_programa.model.Gato;
+
+public class Primeiro_Programa {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+		System.out.println(gato);
+		System.out.println(livro);
+	}
+	
+}
+class Livros {
+	
+	private String nome;
+	private String npag;
+}
